6c2439d0b85226c427fc8eba554754b89f57db19
GoodGuyTobi <arvid.tapper@falun.ntig.se>
2018-09-14 22:21:22 +0200

    Prog2 med treorna
    Vad vi gjorde när vi var med på treornas prog 2 i tvåan

---
+++ DelegateTEST/DelegateTEST/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTEST
+{
+    class Program
+    {
+        delegate void Order();
+
+        static void Main(string[] args)
+        {
+            Cashier();
+        }
+        
+        static void  Cashier()
+        {
+            Order order = null;
+            int choice = -1;
+            Menu();
+
+            while(choice !=0)
+            {
+                Console.Write("Your choice: ");
+                choice = int.Parse(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 1:
+                        order += Burgers;
+                        order += Fries;
+                        order += Drinks;
+                        break;
+
+                    case 2:
+                        order += Burgers;
+                        break;
+
+                    case 3:
+                        order += Fries;
+                        break;
+
+                    case 4:
+                        order += Drinks;
+                        break;
+
+                    case 5:
+                        order = null;
+                        break;
+
+                    case 6:
+                        choice = 0;
+                        break;
+                }
+            }
+
+            order.Invoke();
+
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("----------MENU----------");
+            Console.WriteLine("1. Burgers & Stuff");
+            Console.WriteLine("2. Burgers");
+            Console.WriteLine("3. Fries");
+            Console.WriteLine("4. Drink");
+        }
+
+
+        static void Burgers()
+        {
+            Console.WriteLine($"You ordered a burger.");
+        }
+
+        static void Fries()
+        {
+            Console.WriteLine($"You ordered fries.");
+        }
+
+        static void Drinks()
+        {
+            Console.WriteLine($"You ordered drink.");
+        }
+
+        static void Blabla()
+        {
+            // Retunerer void, 16 overloads.
+            Action<int, int> tal = (x, y) => Console.WriteLine(x*y);
+
+            // Returvärde, 17 overloads
+            Func<int, string> talTillStr = x => $"Blabla {x}";
+
+
+            List<int> siffror = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+            siffror.FindAll(x => x%2 == 2);
+
+        }
+    }
+}
---
+++ Filsparning/Filsparning/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filsparning
+{
+    class Program
+    {
+        static string path = "C:\\C# Filer\\";
+
+        static void Main(string[] args)
+        {
+            WriteTextToFile("Hellu", "text.txt");
+            WriteTextToFile("På dig", "text.txt", true);
+
+            //ReadTextFromFile("text.txt");
+
+
+            SaveToFile("Dogs.blabla");
+            ReadFromFile("Dogs.blabla");
+
+            Console.ReadLine();
+        }
+
+        static void WriteTextToFile(string text, string fileName, bool append = false)
+        {
+            using (StreamWriter writer = new StreamWriter(path + fileName, append))
+            {
+                writer.WriteLine(text);
+            }
+        }
+
+        static void ReadTextFromFile(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(path + fileName))
+            {
+                string text;
+                while ((text = reader.ReadLine()) != null)
+                    Console.WriteLine(text);
+
+                // text = read.ReadToEnd();
+            }
+        }
+
+        /*--------------------------------------*/
+        static void SaveToFile(string fileName)
+        {
+            List<Dog> dogs = new List<Dog>() {
+                    new Dog("Fluffy","Chow Chow"),
+                    new Dog("Steven", "Jack Russel"),
+                    new Dog("Penny", "Shiba Inu")};
+
+            try
+            {
+                using (Stream stream = File.Open(path + fileName, FileMode.Create))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+
+                    bf.Serialize(stream, dogs);
+                }
+            }
+            catch (IOException) { }
+        }
+
+        static void ReadFromFile(string fileName)
+        {
+            try
+            {
+                using (Stream stream = File.Open(path + fileName, FileMode.Open))
+                { 
+                    BinaryFormatter bf = new BinaryFormatter();
+                    List<Dog> dogs = (List<Dog>)bf.Deserialize(stream);
+
+                    foreach (Dog dog in dogs)
+                    {
+                        Console.WriteLine($"{dog.Name} is a {dog.Breed}");
+                    }
+                }
+            }
+            catch (IOException) { }
+        }
+    }
+
+    [Serializable]
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+
+        public Dog(string name, string breed) { Name = name; Breed = breed; }
+    }
+}
